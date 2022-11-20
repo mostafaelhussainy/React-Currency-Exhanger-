ac41306a82bbaebe35b1bd4a9e90a678c96da27e
mostafaelhussainy <mostafaelhussainy22@gmail.com>
2022-11-20 22:02:58 +0200

    passing the data of the top currencies and rendering it

--- src/HomePage/CardsGrid/CardsGrid.tsx
+++ src/HomePage/CardsGrid/CardsGrid.tsx
@@ -1,12 +1,30 @@
 type CardsGridProps = {
   topCurrencies: {}
+  amount: number
 }
 
 function CardsGrid(props: CardsGridProps) {
-  const { topCurrencies } = props
+  const { topCurrencies, amount } = props
+  const filtered: [string, number][] = (Object.entries(topCurrencies));
+  // const currenciesNamesArray:string[] = []
+  // const currenciesexchangeArray:number[] = []
+  // Object.keys(topCurrencies).forEach((currency:string)=>{
+  //   currenciesNamesArray.push(currency)
+  // })
+  // Object.values(topCurrencies).forEach((rate:any)=>{
+  //   currenciesexchangeArray.push(rate)
+  // })
   return ( 
     <>
-      <h1>Cards Grid</h1>
+      <section className="top-currencies">
+        {
+          filtered.map((currency)=>{
+            return (
+              <h1>{currency[0]} : {currency[1] * amount}</h1>
+            )
+          })
+        }
+      </section>
     </>
    );
 }
--- src/HomePage/Home.tsx
+++ src/HomePage/Home.tsx
@@ -112,6 +112,7 @@ function Home() {
       />
       <CardsGrid 
         topCurrencies={topCurrencies}
+        amount={amount}
       />
     </>
   );
